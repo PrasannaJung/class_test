64d97cdac2320fec30e540e45ab165aceec44e68
PrasannaJung <prasannajung08@gmail.com>
2024-05-05 11:12:28 +0545

    Final commit

--- lib/screen/grid_view_screen.dart
+++ lib/screen/grid_view_screen.dart
@@ -1,13 +1,24 @@
 import 'package:flutter/material.dart';
 
 class GridViewScreen extends StatefulWidget {
-  const GridViewScreen({super.key});
+  final String nameData;
+  const GridViewScreen({required this.nameData, super.key});
 
   @override
   State<GridViewScreen> createState() => _GridViewScreenState();
 }
 
 class _GridViewScreenState extends State<GridViewScreen> {
+  late List<String> letters;
+  late List<bool> isClicked;
+
+  @override
+  void initState() {
+    letters = widget.nameData.split('');
+    isClicked = List.generate(widget.nameData.length, (index) => false);
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,8 +28,38 @@ class _GridViewScreenState extends State<GridViewScreen> {
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text("Grid View Screen"),
+      body: GridView.count(
+        crossAxisCount: 3,
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        children: [
+          for (int i = 0; i < letters.length; i++)
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  if (isClicked[i]) {
+                    letters.removeAt(i);
+                    isClicked.removeAt(i);
+                  } else {
+                    isClicked[i] = true;
+                  }
+                });
+              },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.resolveWith<Color>((states) {
+                  if (isClicked[i]) {
+                    return Colors.red;
+                  } else {
+                    return Colors.transparent;
+                  }
+                }),
+              ),
+              child: Text(
+                letters[i],
+                style: const TextStyle(fontSize: 24, fontWeight: FontWeight.w900),
+              ),
+            ),
+        ],
       ),
     );
   }
