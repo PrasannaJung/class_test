306d0dc967993865c45e4e46e412c2fe4728d4c3
PrasannaJung <prasannajung08@gmail.com>
2024-05-05 10:15:23 +0545

    Initial commit

---
+++ android/app/src/main/kotlin/com/prasanna/class_test/class_test/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.prasanna.class_test.class_test
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,33 @@
+import 'dart:async';
+
+import 'package:class_test/screen/grid_view_screen.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    Timer(Duration(seconds: 3),(){
+      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => GridViewScreen()));
+    });
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return  const Scaffold(
+      body: Center(
+        child: Text("Welcome Prasanna"),
+      ),
+    );
+  }
+}
+
